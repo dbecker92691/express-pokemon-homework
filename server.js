040dd508e662ff7c8b3a710687fd90c1a694e85c
Daniel Becker <danielbecker@MacBook-Pro.local>
2018-10-11 07:27:44 -0600

    Edit and Delete added

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express();
 const Pokemon = require('./model/pokemon');
+const methodOverride = require('method-override')
 
 
-// app.get('/pokemon', (req, res) => {
-//     res.send(Pokemon)
-// })
+app.use(methodOverride('_method'))
 
+
+// index route
 app.get('/pokemon', (req, res) =>{
     console.log(req.params)
     res.render('index.ejs', {
@@ -14,13 +15,28 @@ app.get('/pokemon', (req, res) =>{
     })
 })
 
-
-app.get('/pokemon/:index', (req, res) => {
+// show route
+app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', {
         pokemon: Pokemon[req.params.index]
     });
 });
 
+// delete
+
+app.delete('/:id', (req, res) => {
+    Pokemon.splice(req.params.id);
+    res.redirect('/pokemon');
+});
+
+// edit
+
+app.get('/pokemon/:id/edit', (req, res) => {
+    res.render('edit.ejs', {
+        pokemon: Pokemon[req.params.id],
+        id: req.params.id
+    });
+});
 
 
 
@@ -34,3 +50,4 @@ app.get('/pokemon/:index', (req, res) => {
 app.listen(3000, () => {
     console.log('Pokemon app is listening on port 3000');
 })
+module.exports = app
